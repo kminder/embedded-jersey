fe904e28431088bade061a9db75eac86d84d9848
Kevin Minder <kevin.minder@hortonworks.com>
2016-01-07 00:01:12 -0500

    License cleanup.

--- src/test/java/net/minder/poc/JseEmbeddedJerseySampleTest.java
+++ src/test/java/net/minder/poc/JseEmbeddedJerseySampleTest.java
@@ -1,4 +1,4 @@
-package net.minder.poc; /**
+/**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -15,6 +15,7 @@ package net.minder.poc; /**
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package net.minder.poc;
 
 import com.jayway.restassured.RestAssured;
 import org.junit.AfterClass;
